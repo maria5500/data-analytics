1c3e45549e0b3234c7da02a84b1c5a938fd711f7
Maria Pijoan <piju90@hotmail.com>
2024-04-10 16:20:32 +0200

    Script revisat

--- S1/script.sql
+++ S1/script.sql
@@ -5,26 +5,29 @@ SELECT * FROM transactions.company;
 DESCRIBE transaction;
 SELECT * FROM transactions.transaction;
 -- Exercici_2
-SELECT company_name, email, country FROM transactions.company	
+SELECT company_name, email, country
+FROM transactions.company	
 ORDER BY company_name;
 -- Exercici_3
 SELECT DISTINCT company.country
 FROM company
 INNER JOIN transaction ON company.id = transaction.company_id
+WHERE declined != 1
 ORDER BY country;
 -- Exercici_4
 SELECT COUNT(DISTINCT company.country) AS total_country
 FROM company
-INNER JOIN transaction ON company.id = transaction.company_id;
--- Exercici_5
-SELECT company_id, company.country, company.company_name
-FROM company
 INNER JOIN transaction ON company.id = transaction.company_id
-WHERE transaction.company_id = 'b-2354';
+WHERE declined != 1;
+-- Exercici_5
+SELECT id AS company_id, country, company_name
+FROM company
+WHERE id = 'b-2354';
 -- Exercici_6
 SELECT company.company_name, AVG(transaction.amount) AS average_sales
 FROM company
 INNER JOIN transaction ON company.id = transaction.company_id
+WHERE transaction.declined != 1
 GROUP BY company.company_name
 ORDER BY average_sales DESC
 LIMIT 1;
@@ -40,21 +43,21 @@ FROM transaction
 GROUP BY id
 HAVING COUNT(*) > 1;
 -- Exercici_2
-SELECT DATE(timestamp) AS day, MAX(amount) AS max_amount
+SELECT DATE(timestamp) AS day, SUM(amount) AS sum_amount
 FROM transaction
 WHERE declined != 1
 GROUP BY DATE(timestamp)
-ORDER BY max_amount DESC
+ORDER BY sum_amount DESC
 LIMIT 5;
 -- Exercici_3
-SELECT DATE(timestamp) AS day, MIN(amount) AS min_amount
+SELECT DATE(timestamp) AS day, SUM(amount) AS sum_amount
 FROM transaction
 WHERE declined != 1
 GROUP BY DATE(timestamp)
-ORDER BY min_amount ASC
+ORDER BY sum_amount ASC
 LIMIT 5;
 -- Exercici_4
-SELECT country, AVG(amount) AS average_sales
+SELECT country, ROUND(AVG(amount), 2) AS average_sales
 FROM company
 INNER JOIN transaction ON company.id = transaction.company_id
 WHERE declined != 1
@@ -62,13 +65,11 @@ GROUP BY country
 ORDER BY average_sales DESC;
 -- Nivell_3
 -- Exercici_1
-SELECT company_name, phone, country, SUM(amount) AS total_sales
-FROM company
-INNER JOIN transaction ON company.id = transaction.company_id
-WHERE declined != 1
-GROUP BY company_name, phone, country
-HAVING SUM(amount) BETWEEN 100 AND 200
-ORDER BY total_sales DESC;
+SELECT c.company_name, c.phone, c.country, t.amount
+FROM company c
+INNER JOIN transaction t ON c.id = t.company_id
+WHERE t.declined != 1 AND t.amount BETWEEN 100 AND 200
+ORDER BY t.amount DESC;
 -- Exercici_2
 SELECT company_name, DATE(timestamp) AS day_transaction 
 FROM company
